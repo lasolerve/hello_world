3eb354aaca32e1ef324f272c747064958a03d83d
lasolerve <lasolerve@gmail.com>
2017-10-02 18:40:35 +0200

    v1.Clon hello_world

--- hello_world.java
+++ hello_world.java
@@ -1,5 +1,6 @@
 public class hello_world {
 	public static void main(String[] args){
-		System.out.println("Hello world!!");
+		System.out.println("Hello world: To forgive all is as inhuman 
+		as to forgive none (Aristotil). Fortunately we have GIT");
 	}
 }
