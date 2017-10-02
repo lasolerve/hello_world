7f26f27f29704db4b5e5db62632ba9b0d51d123b
lasolerve <lasolerve@gmail.com>
2017-10-02 18:59:56 +0200

    v1.1.Clon hello_world

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,6 @@
 public class hello_world {
 	public static void main(String[] args){
 		System.out.println("Hello world: To forgive all is as inhuman 
-		as to forgive none (Aristotil). Fortunately we have GIT");
+		as to forgive none (Seneca). Fortunately we have GIT");
 	}
 }
